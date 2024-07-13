db9f6aa2fde7cd24b97b7ba802fc0afba29fb723
gabegmbr <221022560@aluno.unb.br>
2024-07-13 15:52:53 -0300

    implemented basic functions

--- script.js
+++ script.js
@@ -1,31 +1,87 @@
-let x, y, operator;
+let x, y, operator, xDecimalPoint, yDecimalPoint, operatorPressed;
+let ans;
 
-function add(x,y){
-    return x+y;
+
+let displayOperation = document.getElementById("displayNum");
+let displayResult = document.getElementById("displayResult");
+let decimalButton = document.getElementById("decimalButton");
+
+function allClear(){
+    x = y = text = '';
+    operatorPressed = xDecimalPoint = yDecimalPoint = false;
+    ans = 0;
+    displayOperation.textContent = "0";
+    displayResult.textContent = "0";
+    decimalButton.disabled = false;
 }
-function subtract(x,y){
-    return x-y;
+
+allClear();
+
+function updateText(){
+    displayOperation.textContent = text;
+    displayResult.textContent = ans;
 }
-function multiply(x,y){
-    return x*y;
+
+function isNumber(){
+    let lastChar = parseInt(text.slice(-1));
+    return !isNaN(lastChar);
 }
-function divide(x,y){
-    return x/y;
+
+function inputOperator(opr){
+    if(opr == '='){
+        operate(operator, parseFloat(x), parseFloat(y));
+        updateText();
+    } else{
+        if(operatorPressed == false && isNumber()){
+            text += opr;
+            operator = opr;
+            updateText();
+            operatorPressed = true;
+        } else{
+            if(isNumber() == false){ //if last inserted char is not a number, change operator (prevents user from adding a sequence of operators)
+                text = text.slice(0, -1);
+                text += opr;
+                operator = opr;
+                updateText();
+            }
+        }
+    }
+}
+
+function input(num){
+    if(operatorPressed == false){
+        text += num;
+        x += num;
+        if(num == '.'){
+            xDecimalPoint = true
+            decimalButton.disabled = true;
+        }
+    } else{
+        if(yDecimalPoint == false) decimalButton.disabled = false;
+        text += num;
+        y += num;
+        if(num == '.'){
+            yDecimalPoint = true;
+            decimalButton.disabled = true;
+        }
+    }
+    updateText();
 }
 
 function operate(operator, x, y){
     switch (operator){
         case '+':
-            add(x,y);
+            ans = x+y
             break;
         case '-':
-            subtract(x,y);
+            ans = x-y;
             break;
         case '*':
-            multiply(x,y);
+            ans = x*y;
             break;
         case '/':
-            divide(x,y);
+            ans = x/y;
             break;
     }
+    return ans;
 }
