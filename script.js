4a3f6c30169f85c7b2cfa8a35d5a137ed79eac16
gabegmbr <221022560@aluno.unb.br>
2024-07-14 10:46:14 -0300

    New functions implemented.
    - AC function
    - Continuous operations

--- script.js
+++ script.js
@@ -22,26 +22,45 @@ function updateText(){
     displayResult.textContent = ans;
 }
 
-function isNumber(){
+function isNumber(){ //function that checks if the last char of the input string is a number.
     let lastChar = parseInt(text.slice(-1));
     return !isNaN(lastChar);
 }
 
+function isStringEmpty(){ //function to check if the input string is empty. basis of conditionals that prevents user from adding an operator before adding a number
+    let char = text.slice(-1);
+    if(char == '') return true;
+    else return false;
+}
+
 function inputOperator(opr){
-    if(opr == '='){
-        operate(operator, parseFloat(x), parseFloat(y));
-        updateText();
+    if(opr == '=' && isStringEmpty() == false){
+        if(y != ''){ //if y is defined, perform operation
+            operate(operator, parseFloat(x), parseFloat(y));
+            updateText();
+        } else{ //else, ans = x. update text.
+            ans = x;
+            updateText();
+        }
     } else{
-        if(operatorPressed == false && isNumber()){
+        if(operatorPressed == false && isNumber() == true){
             text += opr;
             operator = opr;
             updateText();
             operatorPressed = true;
         } else{
-            if(isNumber() == false){ //if last inserted char is not a number, change operator (prevents user from adding a sequence of operators)
+            if(isNumber() == false && isStringEmpty() == false){ //if last inserted char is not a number and the string is not empty, change operator (prevents user from adding a sequence of operators)
                 text = text.slice(0, -1);
                 text += opr;
                 operator = opr;
+                updateText();
+            } else if(isStringEmpty() == false){ //if last char is a number, perform operation
+                operatorPressed = true;
+                operate(operator, parseFloat(x), parseFloat(y));
+                operator = opr;
+                x = ans;
+                y = '';
+                text += opr;
                 updateText();
             }
         }
@@ -50,11 +69,11 @@ function inputOperator(opr){
 
 function input(num){
     if(operatorPressed == false){
-        text += num;
-        x += num;
-        if(num == '.'){
+        text += num; //input text
+        x += num; //
+        if(num == '.'){ 
             xDecimalPoint = true
-            decimalButton.disabled = true;
+            decimalButton.disabled = true; //prevents user from adding another decimal point
         }
     } else{
         if(yDecimalPoint == false) decimalButton.disabled = false;
